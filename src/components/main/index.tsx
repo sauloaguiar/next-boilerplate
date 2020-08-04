c554cddd2adff0dd61d415b247ca52f0b806ca49
Saulo <sauloaguiar89@gmail.com>
2020-08-04 00:42:29 +0000

    Adds configuration for jest, styled components with decorators and storybook

--- src/components/main/index.tsx
+++ src/components/main/index.tsx
@@ -1,7 +1,21 @@
-const Main = () => (
-  <main>
-    <h1>React Avançado</h1>
-  </main>
+import * as S from './styles'
+
+const Main = ({
+  title = 'React Avançado',
+  description = 'Typescript, React js, Next js, Styled Components'
+}) => (
+  <S.Wrapper>
+    <S.Logo
+      src="/img/logo.svg"
+      alt="Imagem de um atomo e react avançado escrito"
+    />
+    <S.Title>{title}</S.Title>
+    <S.Description>{description}</S.Description>
+    <S.Illustration
+      src="/img/hero-illustration.svg"
+      alt="Desevolvedor de frente para tela com codigo"
+    />
+  </S.Wrapper>
 )
 
 export default Main
